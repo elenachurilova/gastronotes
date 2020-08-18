b000df349549f5107ca8f9dcde6643628ec4a591
Elena Churilova <ellena.churilova@gmail.com>
2020-08-17 22:19:40 -0700

    Added show 1 recipe functionality, initial steps for edit functionality

--- static/app.js
+++ static/app.js
@@ -1,48 +1,62 @@
 "use strict";
 
+
+function show_recipe(recipe) {
+
+    //add li to recipes list
+    
+    let li = $(`<li class="recipe_title" id=${recipe.recipe_id}><span class="show">${recipe.recipe_title}</span><span class="edit">(edit)</span></li>`)
+
+    $("#recipes").append(li)
+
+    //upon clicking on li, show recipe inside #whole_recipe
+
+    li.find(".show").on("click", (evt) => {
+
+        evt.preventDefault();
+
+        $("#whole_recipe").empty()
+
+        $("#whole_recipe").append(`<h1 id="recipetitle">${recipe.recipe_title}</h1>
+                                    <ul id="recipeingred">${recipe.recipe_ingred}</ul>
+                                    <ul id="recipedirect">${recipe.recipe_direct}</ul>
+                                    <a href=${recipe.recipe_src} id="recipesrc">${recipe.recipe_src}</a>
+                                    <p id="recipeimage">${recipe.picture_url}</p>`);
+    });
+
+    li.find(".edit").on("click", (evt) => {
+
+        evt.preventDefault();
+
+        $("#recipe_title").val(recipe.recipe_title)
+        $("#recipe_ingred").val(recipe.recipe_ingred)
+        $("#recipe_direct").val(recipe.recipe_direct)
+
+    });
+}
+
 $(".folder_title").on("click", (evt) => {
 
     evt.preventDefault(); 
+
+    $("#recipes").empty()
+    $("#whole_recipe").empty()
 
     let folder_id = evt.target.id
     
     $.get(`/myfolders/myrecipes/${folder_id}`, (data) => {
 
         for (let item of data) {
-            $("#recipe_title").append(`<li class="recipe_title"><a href="/myfolders">${item.recipe_title}</a></li>`)
+
+            show_recipe(item)
         }
-    
-        $(".folder_title").on('click', function() {
-            $(".recipe_title").toggle();
-        });
 
-
-        $(".recipe_title").on("click", (evt) => {
-
-            evt.preventDefault();
-        
-            let folder_id = evt.target.id
-            
-            for (let item of data) {
-
-                $("#whole_recipe").append(`<h1 id="recipetitle">${item.recipe_title}</h1>
-                                            <ul id="recipe_ingred">${item.recipe_ingred}</ul>
-                                            <ul id="recipe_direct">${item.recipe_direct}</ul>
-                                            <a href=${item.recipe_src}id="recipe_src">${item.recipe_src}</a>
-                                            <p id="recipe_image">${item.picture_url}</p>`)
-
-
-            }
-            
-        });
-
-    // <-- closing .get ajax request -->  
     });
-
-// <-- closing event handler --> 
+    
 });
 
+// < ------ ISSUES ------- >
+
+// 4 - need an advice on how to start editing recipes
 
 
-
-// console.log(($("#recipe_title"))[0].textContent)
