1d437363503a86d9e92f6eb6c5ca76808bd23917
Elena Churilova <ellena.churilova@gmail.com>
2020-08-18 18:50:06 -0700

    Updates to AJAX post request to edit a recipe

--- static/app.js
+++ static/app.js
@@ -1,5 +1,39 @@
 "use strict";
 
+function create_form() {
+
+    $("#whole_recipe_edit").append(
+        `<form id="recipe_form">
+            <input id="recipe_id_field" type="hidden">
+            <input type="text" id="recipe_title" name="recipe_title"    placeholder="Recipe Title">
+            <textarea  id="recipe_ingred" name="recipe_ingred"
+            rows="5" cols="33" placeholder="List Of Ingredients"></textarea>
+            <textarea id="recipe_direct" name="recipe_direct" rows="5" cols="33" placeholder="Directions"></textarea>
+            <button id="submit" value="Submit">Submit</button>
+        </form>`
+    );
+
+    $("#submit").on("click", submit_form)
+
+}
+
+function submit_form(evt) {
+
+    evt.preventDefault();
+
+    const formInputs = {
+        'recipe_title' : $("#recipe_title").val(),
+        'recipe_ingred' : $("#recipe_ingred").val(),
+        'recipe_direct' : $("#recipe_direct").val(),
+        'recipe_id' : $("#recipe_id_field").val()
+    }
+
+    $.post('/myfolders/edit', formInputs, (res) => {
+       alert("This was saved!")
+    })
+    
+}
+
 
 function show_recipe(recipe) {
 
@@ -15,10 +49,11 @@ function show_recipe(recipe) {
 
         evt.preventDefault();
 
+        $("#whole_recipe_edit").empty()
         $("#whole_recipe").empty()
 
         $("#whole_recipe").append(`<h1 id="recipetitle">${recipe.recipe_title}</h1>
-                                    <img id="recipeimage" src=${recipe.picture_url}></img>
+                                    <img id="recipeimage" width="400" height="300" src=${recipe.picture_url}></img>
                                     <ul id="recipeingred">${recipe.recipe_ingred}</ul>
                                     <ul id="recipedirect">${recipe.recipe_direct}</ul>
                                     <a href=${recipe.recipe_src} id="recipesrc">${recipe.recipe_src}</a>`);
@@ -27,7 +62,11 @@ function show_recipe(recipe) {
 
     li.find(".edit").on("click", (evt) => {
 
+        $("#whole_recipe_edit").empty()
+
         evt.preventDefault();
+
+        create_form()
 
         $("#recipe_title").val(recipe.recipe_title)
         $("#recipe_ingred").val(recipe.recipe_ingred)
@@ -37,12 +76,15 @@ function show_recipe(recipe) {
     });
 }
 
+//clicking on folder title...
 $(".folder_title").on("click", (evt) => {
 
     evt.preventDefault(); 
 
+    //emptying all previous outputs - folders, recipes, textareas 
     $("#recipes").empty()
     $("#whole_recipe").empty()
+    $("#whole_recipe_edit").empty()
 
     let folder_id = evt.target.id
     
@@ -57,32 +99,8 @@ $(".folder_title").on("click", (evt) => {
     
 });
 
-// upon clicking on Submit button, send ajax POST request to the server /myfolders/edit route
-$("#submit").on("click", (evt) => {
-    evt.preventDefault();
 
-    const formInputs = {
-        'recipe_title' : $("#recipe_title").val(),
-        'recipe_ingred' : $("#recipe_ingred").val(),
-        'recipe_direct' : $("#recipe_direct").val(),
-        'recipe_id' : $("#recipe_id_field").val()
-    }
 
-    $.post('/myfolders/edit', formInputs, (res) => {
-        console.log(res)
-    })
 
-    // $.ajax({
-    //     url: "/myfolders/edit",
-    //     data: $("#recipe_form").serialize(),
-    //     type: "POST",
-    //     success: function(response) {
-    //         console.log(response.success);
-    //     },
-    //     error: function(error) {
-    //         console.log(error);
-    //     }
-    // })
-    
-})
+
 
